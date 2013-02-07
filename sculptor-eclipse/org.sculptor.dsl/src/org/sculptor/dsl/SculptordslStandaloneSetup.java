59dcc142895ae3439374587daacbe1b3d45cffa7
John W. Newman <john.newman.0@gmail.com>
2013-02-07 00:39:37 -0500

    added org.sculptor.dsl project w/ wip_xtext2 branch & all of Torsten's changes merged in
    (build is broken for a moment)

---
+++ sculptor-eclipse/org.sculptor.dsl/src/org/sculptor/dsl/SculptordslStandaloneSetup.java
@@ -0,0 +1,14 @@
+
+package org.sculptor.dsl;
+
+/**
+ * Initialization support for running Xtext languages 
+ * without equinox extension registry
+ */
+public class SculptordslStandaloneSetup extends SculptordslStandaloneSetupGenerated{
+
+	public static void doSetup() {
+		new SculptordslStandaloneSetup().createInjectorAndDoEMFRegistration();
+	}
+}
+
